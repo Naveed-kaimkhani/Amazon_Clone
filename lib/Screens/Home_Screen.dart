7dab2c6974d813a1a5e42d8ce44abc5b254adad1
Naveed kaimkhani <naveedkaimkhami@gmail.com>
2022-06-11 11:57:56 +0500

    fd

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -1,8 +1,8 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:ecommerce_app/AppRoutes.dart';
 import 'package:ecommerce_app/constant/globalVariables.dart';
+import 'package:ecommerce_app/widget/SearchBarWidget.dart';
 import "package:flutter/material.dart";
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Home_Screen extends StatefulWidget {
   const Home_Screen({Key? key}) : super(key: key);
@@ -12,7 +12,9 @@ class Home_Screen extends StatefulWidget {
 }
 
 class _Home_ScreenState extends State<Home_Screen> {
-//  var index=0;
+  bool isReadOnly = true;
+  bool hasBackButton = false;
+
   List<Widget> names = [
     const Icon(
       Icons.home_outlined,
@@ -33,52 +35,41 @@ class _Home_ScreenState extends State<Home_Screen> {
   ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar:PreferredSize(
-                preferredSize: Size.fromHeight(80.h), // here the desired height
-                child: Container(
-                //height: 20.h,
-     // width: double.infinity,
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              colors: globalVariables.backgroundGradient,
-              begin: Alignment.centerLeft,
-              end: Alignment.centerRight)),
-    ),
-            ),
-      body: const Center(
-        child: Text("Home Screen"),
-      ),
-      bottomNavigationBar: CurvedNavigationBar(
-       //// index: index,
-        //backgroundColor:Colors.transparent,
-      //  onTap:(index)=>setState(() =>this.index=index),
-        height: 60,
-        //  index:ind;
-        // backgroundColor: bluish,
-        color: globalVariables.kPrimaryColor,
-        buttonBackgroundColor: Colors.transparent,
-        backgroundColor: Colors.transparent,
-        items: names,
-        onTap: (index) {
-          if (index == 0) {
-            setState(() {
-              Navigator.of(context).pushNamed(AppRoutes.HomeScreen);
-            });
-          }
-          if (index == 1) {
-            setState(() {
-              Navigator.of(context).pushNamed(AppRoutes.HomeScreen);
-            });
-          }
-          if (index == 3) {
-            setState(() {});
-          }
-          if (index == 4) {
-            setState(() {});
-          }
-        }
-      )
-      );
+    return SafeArea(
+      child: Scaffold(
+          appBar:SearchBarWidget(isReadOnly: true, hasBackButton:false),
+          body: const Center(
+            child: Text("Home Screen"),
+          ),
+          bottomNavigationBar: CurvedNavigationBar(
+              //// index: index,
+              //backgroundColor:Colors.transparent,
+              //  onTap:(index)=>setState(() =>this.index=index),
+              height: 60,
+              //  index:ind;
+              // backgroundColor: bluish,
+              color: globalVariables.kPrimaryColor,
+              buttonBackgroundColor: Colors.transparent,
+              backgroundColor: Colors.transparent,
+              items: names,
+              onTap: (index) {
+                if (index == 0) {
+                  setState(() {
+                    Navigator.of(context).pushNamed(AppRoutes.HomeScreen);
+                  });
+                }
+                if (index == 1) {
+                  setState(() {
+                    Navigator.of(context).pushNamed(AppRoutes.HomeScreen);
+                  });
+                }
+                if (index == 3) {
+                  setState(() {});
+                }
+                if (index == 4) {
+                  setState(() {});
+                }
+              })),
+    );
   }
 }
---
+++ lib/widget/SearchBarWidget.dart
@@ -0,0 +1,90 @@
+import 'package:ecommerce_app/constant/Utils.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ecommerce_app/constant/globalVariables.dart';
+
+class SearchBarWidget extends StatelessWidget with PreferredSizeWidget {
+  final bool isReadOnly;
+  final bool hasBackButton;
+  SearchBarWidget({
+    Key? key,
+    required this.isReadOnly,
+    required this.hasBackButton,
+  })  : preferredSize = const Size.fromHeight(80),
+        super(key: key);
+
+  @override
+  final Size preferredSize;
+
+  OutlineInputBorder border = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(7),
+    borderSide: const BorderSide(
+      color: Colors.grey,
+      width: 1,
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = Utils.getScreenSize();
+    return Container(
+      height: 20.h,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: globalVariables.backgroundGradient,
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          hasBackButton
+              ? IconButton(
+                  onPressed: () {
+                    //Navigator.pop(context);
+                  },
+                  icon: const Icon(Icons.arrow_back))
+              : Container(),
+          SizedBox(
+            width: screenSize.width * 0.7,
+            child: Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    blurRadius: 8,
+                    spreadRadius: 1,
+                    offset: const Offset(0, 5),
+                  ),
+                ],
+              ),
+              child: TextField(
+                onSubmitted: (String query) {
+                 
+                },
+                readOnly: isReadOnly,
+                onTap: () {
+                  if (isReadOnly) {
+                  
+                  }
+                },
+                decoration: InputDecoration(
+                  hintText: "Search Products",
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: border,
+                  focusedBorder: border,
+                ),
+              ),
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.mic_none_outlined),
+          )
+        ],
+      ),
+    );
+  }
+}
